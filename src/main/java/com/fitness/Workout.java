076baf6659e19ded0199650082a217b1c27c89b6
aantle11 <alicia.antle@keyin.com>
2025-10-02 20:39:41 -0230

    workout.java class complete

--- src/main/java/com/fitness/Workout.java
+++ src/main/java/com/fitness/Workout.java
@@ -1,4 +1,30 @@
 package com.fitness;
 
+import java.time.LocalDate;
+
 public class Workout {
+    private String type;
+    private int duration;
+    private int caloriesBurned;
+    private LocalDate date;
+
+    public Workout(String type, int duration, int caloriesBurned, LocalDate date) {
+        if (duration <= 0 || caloriesBurned < 0) {
+            throw new IllegalArgumentException("Invalid workout details..");
+        }
+        this.type = type;
+        this.duration = duration;
+        this.caloriesBurned = caloriesBurned;
+        this.date = date;
+    }
+
+    public String getType() {return type;}
+    public int getDuration() {return duration;}
+    public int getCaloriesBurned() {return caloriesBurned;}
+    public LocalDate getDate() {return date;}
+
+    @Override
+    public String toString() {
+        return type + "(" + duration + "," + caloriesBurned + "cal)";
+    }
 }
